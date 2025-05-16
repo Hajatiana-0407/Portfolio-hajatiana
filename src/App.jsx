9dbb5a99a1d142654ba96ec887185083669493c7
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-05-16 18:39:52 +0300

    Theme configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import NavBar from './Components/NavBar'
 import Home from './pages/Home'
-import Services from './pages/Services'
+import About from './pages/About'
+import Service from './pages/Service'
+import Project from './pages/Project'
+import { useAppContext } from './context/AppContext'
+import Skill from './pages/Skill'
+import Contact from './pages/Contact'
+import Button from './Components/Button'
+import { FaArrowUp } from 'react-icons/fa'
+import Footer from './Components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { activeOnglet, setActiveOnglet } = useAppContext();
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveOnglet(entry.target.id);
+          }
+        });
+      },
+      {
+        rootMargin: '-50% 0px -50% 0px',
+        threshold: 0,
+      }
+    );
+
+    const sections = document.querySelectorAll('section');
+    sections.forEach((section) => observer.observe(section));
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [setActiveOnglet]);
+
+
 
   return (
     <>
-      <div className='bg-background'>  
-          <NavBar/>
-          <Home/>
-          <Services/>
+      <div className='accueil-bg bg-fixed min-h-screen'>
+        <div className='font-body'>
+          <NavBar />
+          <Home />
+          <Service />
+          <Project />
+          <Skill />
+          <About />
+          <Contact />
+          <Footer />
+        </div>
+
+        {activeOnglet !== 'accueil' &&
+          <div className='fixed z-40 bottom-20 right-15 w-[max-content]'>
+            <Button href='#accueil' icon={<FaArrowUp />} />
+          </div>
+        }
       </div>
     </>
   )
